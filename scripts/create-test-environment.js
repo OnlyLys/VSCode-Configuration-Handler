0cd4a02bbcb5549f01a13b2b448c4e38126d33ab
L.Y. Sim <onlylys@protonmail.com>
2019-03-15 23:33:41 +0800

    Script changes...
    1. Rename shelljs 'launch test' script.
    2. Replace temporary test environment with a permanent one that is bundled with the repository.

--- scripts/create-test-environment.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-var shell = require('shelljs');
-
-// Make a copy of the template test environment containing the multi-root workspace
-shell.rm('-rf', './.test-environment');
-shell.cp('-r', './.test-environment-template', './.test-environment');
--- scripts/delete-test-environment.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-var shell = require('shelljs');
-
-// Delete the temporary `/.test-environment` directory containing the multi-root workspace
-shell.rm('-rf', './.test-environment');

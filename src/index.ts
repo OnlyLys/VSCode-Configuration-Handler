d7f4ca4a7f56e3184636d17962aff432e39cd97e
L.Y. Sim <onlylys@protonmail.com>
2020-01-27 18:05:30 +0800

    Minor comment fixes.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// Expose all of these for users of this package
+// Errors exposed for users of this package.
 export { ConfigurationHandler } from './configuration-handler';
 export { ConfigurationHandlerCompat } from './configuration-handler-compat';
 export { ConfigurationBadDefaultError, ConfigurationNameEmptyError } from './errors';
